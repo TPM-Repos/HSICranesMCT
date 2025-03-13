0859356fe6eb9bfc437fe1a035cc8d71d045a14e
Joseph C Caswell <josephcaswell@tpm.com>
2025-03-13 13:00:38 -0400

    Create PWA

---
+++ sw.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = 'hsi-mct-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './projects.html',
+  './history.html',
+  './drive-apps.html',
+  './details.html',
+  './run.html',
+  './dist/css/core.css',
+  './dist/css/header.css',
+  './dist/css/reset.css',
+  './dist/js/core.js',
+  './dist/js/header.js',
+  './dist/js/ui.js',
+  './dist/fonts/Inter/InterVariable.woff2',
+  './dist/fonts/Roboto/RobotoFlex.woff2',
+  './dist/img/HSI-Background.webp',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png',
+  './favicon.ico'
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate service worker and clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+// Fetch assets from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            // Don't cache responses that aren't successful
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            // Clone the response since it can only be consumed once
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
